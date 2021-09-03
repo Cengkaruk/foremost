5f6b014d4bc2e77ed2a7f2caa07eef2f02a62b0c
Aji Kisworo Mukti <adzy.maniac@gmail.com>
2021-09-03 09:04:34 +0700

    feat(interface): Interface for IWETH

---
+++ contracts/interfaces/IWETH.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+interface IWETH {
+  function deposit() external payable;
+
+  function withdraw(uint256 wad) external;
+
+  function transfer(address to, uint256 value) external returns (bool);
+}
